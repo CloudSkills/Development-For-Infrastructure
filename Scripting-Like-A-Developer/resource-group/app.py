975e5bb63fc11370d6cee4cf49da00d8000c1005
Michael Levan <mlevan1992@gmail.com>
2021-01-05 09:04:59 -0500

    app

--- Scripting-Like-A-Developer/resource-group/app.py
+++ Scripting-Like-A-Developer/resource-group/app.py
@@ -1,14 +1,27 @@
 from azure.mgmt.resource import ResourceManagementClient
+from azure.identity import AzureCliCredential
+
+import sys
 
 
-credential = "some cred"
-subID = "some sub ID"
+def main():
+    create_resource_group(subID)
 
-client = ResourceManagementClient(credential, subID)
+def create_resource_group(subID):
+    credential = AzureCliCredential()
 
-client.resource_groups.create_or_update(
-    resource_group_name = "someresourcegroup",
-    parameters = {
-        "location": "eastus"
-    }
-)
+    client = ResourceManagementClient(credential, subID)
+
+    client.resource_groups.create_or_update(
+        resource_group_name = name,
+        parameters = {
+            "location": location
+        }
+    )
+
+subID = sys.argv[1]
+name = sys.argv[2]
+location = sys.argv[3]
+
+if __name__ == '__main__':
+    main()
